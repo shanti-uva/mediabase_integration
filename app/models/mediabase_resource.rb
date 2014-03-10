497428347493a094fbb629655ca0df0757ee5ac4
Andres Montano <amontano@virginia.edu>
2014-03-10 10:41:33 -0600

    changing URL for mediabase

--- app/models/mediabase_resource.rb
+++ app/models/mediabase_resource.rb
@@ -1,5 +1,5 @@
 class MediabaseResource < ActiveResource::Base
-  self.site = 'http://mediabase.drupal.shanti.virginia.edu'
+  self.site = 'http://mediabase.shanti.virginia.edu'
   self.timeout = 100
   self.format = :xml
 end
